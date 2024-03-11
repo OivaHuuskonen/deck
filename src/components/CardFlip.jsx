e49464bbe1a2fa572a50b90e6de4a67e8f975fa8
OivaHuuskonen <Solarsystems3@gmail.com>
2024-03-11 20:30:25 +0200

    zzz

--- src/components/CardFlip.jsx
+++ src/components/CardFlip.jsx
@@ -278,7 +278,7 @@ const CardFlip = ({ title, content, title2, content2 }) => {
 
 export default CardFlip;*/
 
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const CardFlip = ({ title, content, title2, content2 }) => {
@@ -293,24 +293,24 @@ const CardFlip = ({ title, content, title2, content2 }) => {
     }
     
     return (
-         <div className="flex flex-col gap-6 group relative shadow-lg text-[#000000] rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer z-20" onClick={handleFlip}>
+         <div className="flex flex-col gap-6 group relative shadow-lg text-[#000000] rounded-xl px-6 py-8  overflow-hidden cursor-pointer z-20" onClick={handleFlip}>
          <motion.div
             className="flip-card-inner w-full h-full"
-            style={{ width: '250px', height: '370px' }}
+           // style={{ width: '250px', height: '370px' }}
             initial={false}
-            animate={{ rotateY: isFlipped ? 180 : 360 }}
+            animate={{ rotateY: isFlipped ? 180 : 0 }}
             transition={{ duration: 0.6, animationDirection: "normal" }}
             onAnimationComplete={() => setIsAnimating(false)}
     
         >
-            <div className="flip-card-front w-full h-full bg-cover border-[1px] border-[#000000]  text-[#000000]rounded-lg p-4 flex flex-col justify-center items-center">
+            <div className="flip-card-front w-full h-full bg-cover border-[1px] border-[#000000]  text-[#000000]rounded-lg p-4">
                 <div className="absolute inset-0 bg-[#FFFAF0] opacity-10 group-hover:opacity-50" />
                 <div className="relative flex flex-col gap-3 text-center">
                     <h1 className="text-xl lg:text-2xl">{title}</h1>
                     <p className="lg:text-base">{content}</p>
                 </div>
             </div>
-            <div className="flip-card-back w-full h-full bg-cover border-[1px] border-[#000000]  text-[#000000] rounded-lg p-4 flex flex-col justify-center items-center">
+            <div className="flip-card-back w-full h-full bg-cover border-[1px] border-[#000000]  text-[#000000] rounded-lg p-4">
                 <div className="absolute inset-0 bg-[#FFFAF0] opacity-10 group-hover:opacity-50" />
                 <div className="relative flex flex-col gap-3 text-center">
                     <h1 className="text-xl lg:text-2xl">{title2}</h1>
@@ -325,7 +325,7 @@ const CardFlip = ({ title, content, title2, content2 }) => {
 export default CardFlip;
 
 
-/*import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const CardFlip = ({ title, content, title2, content2, backgroundImage, icon }) => {
@@ -370,6 +370,57 @@ const CardFlip = ({ title, content, title2, content2, backgroundImage, icon }) =
 };
 
 export default CardFlip;*/
+
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const CardFlip = ({ title, content, title2, content2 }) => {
+    const [isFlipped, setIsFlipped] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    function handleFlip() {
+        if (!isAnimating) {
+            setIsFlipped(!isFlipped);
+            setIsAnimating(true);
+        }
+    }
+
+    return (
+         <div className="flex flex-col gap-6 group relative shadow-lg text-[#000000] rounded-xl px-6 py-8 h-[250px] w-[270px] lg:h-[400px] lg:w-[370px] overflow-hidden cursor-pointer z-20" onClick={handleFlip}>
+         <motion.div
+            className="flip-card-inner w-full h-full"
+
+            style={{ width: '100%', height: '100%' }}
+            //style={{ width: '250px', height: '370px' }}
+            initial={false}
+            animate={{ rotateY: isFlipped ? 180 : 360 }}
+            transition={{ duration: 0.6, animationDirection: "normal" }}
+            onAnimationComplete={() => setIsAnimating(false)}
+        >
+            <div className="flip-card-front w-full h-full bg-cover border-[1px] border-[#000000] text-[#000000]rounded-lg p-4 flex flex-col justify-center items-center">
+                <div className="absolute inset-0 bg-[#FFFAF0] opacity-10 group-hover:opacity-50" />
+                <div className="relative flex flex-col gap-3 text-center">
+                <h1 className="text-base lg:text-xl sm:text-lg">{title}</h1>
+                        <p className="lg:text-sm sm:text-sm">{content}</p>
+                    {/*<h1 className="text-base lg:text-1xl">{title}</h1>
+                    <p className="lg:text-sm">{content}</p>*/}
+                </div>
+            </div>
+            <div className="flip-card-back w-full h-full bg-cover border-[1px] border-[#000000]  text-[#000000] rounded-lg p-4 flex flex-col justify-center items-center">
+                <div className="absolute inset-0 bg-[#FFFAF0] opacity-10 group-hover:opacity-50" />
+                <div className="relative flex flex-col gap-3 text-center">
+                <h1 className="text-base lg:text-xl sm:text-lg">{title2}</h1>
+                        <p className="lg:text-sm sm:text-sm">{content2}</p>
+                    {/*<h1 className="text-base lg:text-1xl">{title2}</h1>
+                    <p className="lg:text-sm">{content2}</p>*/}
+                </div>
+            </div>
+        </motion.div>
+        </div>
+    );
+};
+
+export default CardFlip;
 
 
 
